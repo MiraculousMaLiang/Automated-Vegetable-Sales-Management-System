fdc856ffeb5b1daa788cb3ab54cc704f7198ee68
BuildTools <unconfigured@null.spigotmc.org>
2025-11-14 14:22:55 +0800

    更改了user.java 给password添加了@JsonIgnore注解

--- src/main/java/com/vegetable/entity/User.java
+++ src/main/java/com/vegetable/entity/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -32,6 +33,7 @@ public class User implements Serializable {
     private String phone;
 
     /** 密码(SHA256加密) */
+    @JsonIgnore
     private String password;
 
     /** 头像URL */
